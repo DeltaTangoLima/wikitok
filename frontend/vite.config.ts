cc95dd344c440dfb10617b5d2e10cc1426d0a89a
Dan Lehman <dan@leh.mn>
2025-02-09 22:59:29 +1100

    Trying to get env var working for server.alowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,36 +5,35 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "WikiTok",
-        short_name: "WikiTok",
-        icons: [
-          {
-            src: "/wiki-logo.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-          },
-        ],
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-      },
-    }),
-  ],
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    // vite config
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "WikiTok",
+          short_name: "WikiTok",
+          icons: [
+            {
+              src: "/wiki-logo.svg",
+              sizes: "any",
+              type: "image/svg+xml",
+            },
+          ],
+          start_url: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#000000",
+        },
+      }),
+    ],
     define: {
       __ALLOWED_HOSTS__: JSON.stringify(env.VITE_SERVER_ALLOWED_HOSTS),
     },
+    server: {
+      allowedHosts: __ALLOWED_HOSTS__
+    },
   },
-	server: {
-		allowedHosts: __ALLOWED_HOSTS__
-	},
 });
